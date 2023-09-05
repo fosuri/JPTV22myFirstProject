2ca31a5bf04dfcded7a52dc7f73a7cfe50b4285d
pupil <pupil@ivkhk.ee>
2023-09-05 10:38:54 +0300

    first commit

---
+++ src/jptv22myfirstproject/JPTV22MyFirstProject.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv22myfirstproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class JPTV22MyFirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); {
+        System.out.println("Enter your nam: ");
+        String firstname = scanner.nextLine();
+        System.out.println("Enter your last name");
+        String lastname = scanner.nextLine();
+        System.out.println("Hello" + " " + firstname + " " + lastname+"!");
+        scanner.close();  
+        // TODO code application logic here
+        }
+    }
+    
+}
